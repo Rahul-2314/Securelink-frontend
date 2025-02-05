dbf350f715ae267b4822ef8e6b98e375a0c078e1
Mr. Rahul Chowdhury <96120918+Rahul-2314@users.noreply.github.com>
2025-02-05 13:29:59 +0530

    Update Mylinks.jsx

--- src/components/mylinks/Mylinks.jsx
+++ src/components/mylinks/Mylinks.jsx
@@ -46,16 +46,16 @@ function Mylinks() {
 
 	const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-	if (loading) {
-		return (
-			<div className="loading_loader">
-				<i className="fa-solid fa-spinner fa-spin"></i>
-			</div>
-		);
-	}
+	// if (loading) {
+	// 	return (
+	// 		<div className="loading_loader">
+	// 			<i className="fa-solid fa-spinner fa-spin"></i>
+	// 		</div>
+	// 	);
+	// }
 
 	if (error) {
-		return <div>Error: {error}</div>;
+		return <div className="mylinks" style="text-align: center">Error: {error}</div>;
 	}
 
 	return (
@@ -63,42 +63,52 @@ function Mylinks() {
 			<div className="mylinks">
 				<h2 className="heading">---- Mylinks : Link History ----</h2>
 
-				<div className="scrollable-container">
-					<div className="scrollable-content">
-						{mylinks.map((linkId, index) => {
-							const analyticsData = analytics[linkId];
-							return (
-								<div key={index} className="content">
-									<p className="sn">{index + 1}.</p>
-									<span className="link">
-										<a
-											href={`${API_BASE_URL}/${analyticsData?.shortUrl}` || "#"}
-											target="_blank"
-											rel="noopener noreferrer"
-										>
-											<i className="fa-solid fa-lock"></i>{" "}
-											{`${API_BASE_URL}/${analyticsData?.shortUrl}` || (
+				{loading ? (
+					<div className="loading_loader">
+						<i className="fa-solid fa-spinner fa-spin"></i>
+					</div>
+				) : (
+					<div className="scrollable-container">
+						<div className="scrollable-content">
+							{mylinks.map((linkId, index) => {
+								const analyticsData = analytics[linkId];
+								return (
+									<div key={index} className="content">
+										<p className="sn">{index + 1}.</p>
+										<span className="link">
+											<a
+												href={
+													`${API_BASE_URL}/${analyticsData?.shortUrl}` || "#"
+												}
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												<i className="fa-solid fa-lock"></i>{" "}
+												{`${API_BASE_URL}/${analyticsData?.shortUrl}` || (
+													<i className="fa-solid fa-spinner fa-spin"></i>
+												)}
+											</a>
+										</span>
+										<i
+											className="fas fa-copy"
+											onClick={() => copyURL(analyticsData?.shortUrl)}
+											aria-label="Copy URL"
+										></i>
+										<p className="clicks">
+											Total Clicks:{" "}
+											{analyticsData?.clicks ?? (
 												<i className="fa-solid fa-spinner fa-spin"></i>
 											)}
-										</a>
-									</span>
-									<i
-										className="fas fa-copy"
-										onClick={() => copyURL(analyticsData?.shortUrl)}
-										aria-label="Copy URL"
-									></i>
-									<p className="clicks">
-										Total Clicks:{" "}
-										{analyticsData?.clicks ?? (
-											<i className="fa-solid fa-spinner fa-spin"></i>
-										)}
-									</p>
-								</div>
-							);
-						})}
+										</p>
+									</div>
+								);
+							})}
+						</div>
 					</div>
-				</div>
-				<ToastContainer position="bottom-right" transition={Zoom} />
+				)}
+				<strong>
+					<ToastContainer position="bottom-right" transition={Zoom} />
+				</strong>
 			</div>
 		</section>
 	);
