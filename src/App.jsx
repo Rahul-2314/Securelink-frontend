31c5aa8a6e6e58ac6a07d17359ee83356f790a25
Mr. Rahul Chowdhury <96120918+Rahul-2314@users.noreply.github.com>
2025-03-17 23:49:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Navbar from './components/navbar/Navbar'
 import Homepage from './components/homepage/Homepage'
 import Footer from './components/footer/Footer'
